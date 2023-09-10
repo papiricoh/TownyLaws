cfee7a629744b291a98e97a666ddcff9b3dabedc
Papiricoh <slendeman0200@gmail.com>
2023-09-10 23:07:26 +0200

    first commit

---
+++ src/main/java/org/papiricoh/townylaws/TownyLaws.java
@@ -0,0 +1,17 @@
+package org.papiricoh.townylaws;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TownyLaws extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

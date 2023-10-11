3661c348c30e7210336d2bf222a6d7c954988623
Papiricoh <slendeman0200@gmail.com>
2023-10-12 00:15:25 +0200

    Changed DataParser.java to DataLoader.java

--- src/main/java/org/papiricoh/townylaws/TownyLaws.java
+++ src/main/java/org/papiricoh/townylaws/TownyLaws.java
@@ -1,12 +1,11 @@
 package org.papiricoh.townylaws;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import org.papiricoh.townylaws.data.DataParser;
+import org.papiricoh.townylaws.data.DataLoader;
 import org.papiricoh.townylaws.data.DatabaseManager;
 import org.papiricoh.townylaws.object.government.Government;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public final class TownyLaws extends JavaPlugin {
@@ -18,11 +17,11 @@ public final class TownyLaws extends JavaPlugin {
         this.db = new DatabaseManager();
         try {
             this.db.connect(this);
-            DataParser.initializeDatabase(this.db.getConnection());
+            DataLoader.initializeDatabase(this.db.getConnection());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        this.governments = DataParser.loadDatabase();
+        this.governments = DataLoader.loadDatabase();
 
     }
 
--- src/main/java/org/papiricoh/townylaws/data/DataLoader.java
+++ src/main/java/org/papiricoh/townylaws/data/DataLoader.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class DataParser {
+public class DataLoader {
     public static List<Government> loadDatabase() {
         //TownyLaws.db.getConnection();
         ArrayList<Government> governments = new ArrayList<>();
